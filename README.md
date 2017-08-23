184657513a682a340c53dcb452f0309cb2e321ca
adrianmarello <adrianmarello@gmail.com>
2017-08-23 12:50:06 -0300

    cambios minimos

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # workshop-apirest
 Ejemplo básico para consumir un API REST usando la API de Turismo Catamarca.
+
+
+$.get('https://sgtur.catamarca.gob.ar/api/v1/hospedaje/',function(data,status,xhr){
+	console.log(data);
+	console.log(status);
+	console.log(xhr);
+})
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,29 +4,38 @@ $(document).ready(function() {
     var lista = $('#hospedajes-list .list-group');
     var items = $('#hospedajes-list .list-group a');
 
-    function clearList() {
-        lista.html('');
+    // FUNCTIONS
+    function limpiarLista(clear) {
+        if (clear){
+            lista.html("");
+        }else{
+            lista.html("<a class='list-group-item sin-datos'><strong>No hay datos</strong></a>");
+        }
     }
 
     function consultarHospedajes() {
         $.ajax({
+            method: "GET",
             url: 'https://sgtur.catamarca.gob.ar/api/v1/hospedaje/',
+            timeout: 20000, 
             success: function(result) {
-                clearList();
+                limpiarLista(true);
                 $.each(result, function(i, item){
-                    console.log(i);
-                    console.log(item);
-                    lista.append("<a class='list-group-item'><strong>" + item.nombre + ", <span class='span-domicilio'>" + item.domicilio + "</span></strong></a>");
+                    lista.append("<a class='list-group-item item-hospedaje'><strong>" + item.nombre + ", <span class='span-domicilio'>" + item.domicilio + "</span></strong></a>");
                 })
+            },
+            error: function(result){
+                console.log(result);
             }
         });
     }
 
+    // EVENTS
     $('.btn-consulta-hospedaje').click(function(){
         consultarHospedajes();
     });
     $('.btn-limpiar').click(function(){
-        clearList();
+        limpiarLista(false);
     });
 
 

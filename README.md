3ffc0594441039bb30c03f61fc319a6db8592e30
adrianmarello <adrianmarello@gmail.com>
2017-09-02 01:21:36 -0300

    Final Code

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
 # workshop-apirest
-Ejemplo básico para consumir un API REST usando la API de Turismo Catamarca.
+Ejemplo básico para consumir un API REST.
+
+# Proyecto en github
+https://github.com/colmenacoop/workshop-apirest.git
+
+# JQUERY links de interés
+http://api.jquery.com/jquery.ajax/
+https://api.jquery.com/jquery.get/
 
 
-$.get('https://sgtur.catamarca.gob.ar/api/v1/hospedaje/',function(data,status,xhr){
-	console.log(data);
-	console.log(status);
-	console.log(xhr);
-})
+
+XMLHttpRequest
+
+Es una interfaz empleada para realizar peticiones HTTP y HTTPS a servidores web.
+Para los datos transferidos se usa cualquier codificación basada en texto, incluyendo: texto plano, XML, JSON, HTML.
+La interfaz se implementa como una clase de la que la aplicación cliente puede generar tantas instancias como necesite para manejar el dialogo con el servidor.
+El uso más popular de esta interfaz es proporcionar contenido dinámico y actualizaciones asíncronas en páginas web mediante tecnologías construidas sobre ella como por ejemplo AJAX.
+
+
+
+Top 10 HTTP status codes
+
+•	200 Ok - La solicitud es correcta (esta es la respuesta estándar para las solicitudes HTTP satisfactorias).
+•	201 Created - La solicitud se ha cumplido y se crea un nuevo recurso.
+•	204 No Content - La solicitud se ha procesado correctamente, pero no está devolviendo ningún contenido.
+•	400 Bad Request - La solicitud no se puede cumplir debido a la mala sintaxis.
+•	403 Forbidden - La solicitud era una solicitud legal, pero el servidor se niega a responder a ella.
+•	401 Unauthorized - La solicitud era una solicitud legal, pero el servidor se niega a responder a ella. Para usar cuando la autenticación es posible pero ha fallado o aún no se ha proporcionado.
+•	404 Not Found - La página solicitada no se pudo encontrar pero puede estar disponible de nuevo en el futuro.
+•	405 Method Not Allowed - Se realizó una solicitud de una página utilizando un método de solicitud no soportado por esa página.
+•	409 Conflict - indica que se ha detectado un cambio conflictivo durante un intento de modificar un recurso.
+•	500 Internal Server Error - Un mensaje de error genérico, dado cuando no es adecuado ningún mensaje específico.
+
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,52 +1,151 @@
 $(document).ready(function() {
 
     // INIT VARS
-    var $lista = $('#hospedajes-list .list-group');
+    var apiURL = 'http://localhost:8000/api/v1/asistente/';
+    var $list = $('#list');
+    var $loadingList = $('.loadingList');
+    var $loadingForm = $('.loadingForm');
+    var $inputName = $('#name');
+    var $inputLastname = $('#lastname');
+    var $inputDNI = $('#dni');
+    var resourceID = null;
+    
+    // Muestro la URL de la API en el index.html
+    $('#api-rest strong').text('API REST:' + apiURL);
 
-    // FUNCTIONS
-    function limpiarLista(clear) {
-        if (clear){
-            $lista.html("");
+
+    // FUNCTIONS TO MODIFY HTML DOM
+
+    function modifyList(text) {
+        if (!text){
+            $list.html("");
         }else{
-            $lista.html("<a class='list-group-item sin-datos'><strong>No hay datos</strong></a>");
+            $list.html("<a class='list-group-item sin-datos'><strong>" + text + "</strong></a>");
         }
     }
 
-    function consultarHospedajes() {
-        $.ajax({
-            method: "GET",
-            url: 'https://sgtur.catamarca.gob.ar/api/v1/hospedaje/',
-            timeout: 20000, 
-            success: function(result) {
-                limpiarLista(true);
-                $.each(result, function(i, item){
-                    $lista.append("<a class='list-group-item item-hospedaje'><strong>" + item.nombre + ", <span class='span-domicilio'>" + item.domicilio + "</span></strong></a>");
-                })
-            },
-            error: function(result){
-                console.log(result);
-            }
-        });
+    function addItem(item) {
+        $list.prepend(
+            "<a class='list-group-item item' data-id='" + item.pk + "'>" +
+                "<strong>"+ 
+                    "<span class='span-name'>" + item.nombre + "</span>, "+
+                    "<span class='span-lastname'>" + item.apellido + "</span>, "+
+                    "<span class='span-dni'>" + item.dni + "</span>"+
+                "</strong>"+
+                "&nbsp;"+
+                "<button class='btn-editar'>editar</button>"+
+                "<button class='btn-eliminar'>eliminar</button>"+
+            "</a>"
+        );
     }
 
+    function removeItem() {
+        $('#list .item[data-id="'+ resourceID +'"]').remove();
+    }
+
+    function updateItem(item) {
+        removeItem();
+        addItem(item);
+    }
+
+
+    // FUNCTIONS WITH AJAX REQUESTS
+
+    function getResource() {
+        // AJAX GET METHOD
+        console.log('Inserte su metodo metodo AJAX GET aquí');
+    }
+
+    function postResource() {
+        // DATA AND LOG
+        var datos = {nombre: $inputName.val(), apellido: $inputLastname.val(), dni: $inputDNI.val()};
+        console.log(datos);
+
+        // AJAX POST METHOD
+        console.log('Inserte su metodo metodo AJAX POST aquí');
+    }
+
+    function putResource() {
+        // DATA AND LOG
+        var datos = {nombre: $inputName.val(), apellido: $inputLastname.val(), dni: $inputDNI.val()};
+        console.log('Se modificara el recurso con ID: ' + resourceID);
+        console.log(datos);
+
+        // AJAX PUT METHOD
+        console.log('Inserte su metodo metodo AJAX PUT aquí');
+    }
+
+    function deleteResource() {
+        // LOG
+        console.log('Se eliminara el recurso con ID: ' + resourceID);
+
+        // AJAX DELETE METHOD
+        console.log('Inserte su metodo metodo AJAX DELETE aquí');
+    }
+
+
     // EVENTS
-    $('.btn-consulta-hospedaje').click(function(){
-        consultarHospedajes();
+    $('.btn-consulta').click(function(){
+        getResource();
     });
     $('.btn-limpiar').click(function(){
-        limpiarLista(false);
+        modifyList('No hay datos');
+    });
+    
+    $('.btn-crear').click(function(event){
+        // Abro el modal.
+        $("#myModal").modal();
+
+        // Cambio el título del modal.
+        $('.modal-title').text('Agregar Recurso');
+
+        // Muestro el boton para crear y escondo el boton para editar
+        $('.btn-crear-confirmar').show();
+        $('.btn-editar-confirmar').hide();
+
+        // Limpio los input que se encuentran en el modal.
+        $inputName.val('');
+        $inputLastname.val('');
+        $inputDNI.val('');
+    });
+    
+    $list.on('click', '.btn-editar', function(event){
+        // Abro el modal
+        $("#myModal").modal();
+
+        // Cambio el título del modal.
+        $('.modal-title').text('Modificar Recurso');
+
+        // Muestro el boton para editar y escondo el boton para crear
+        $('.btn-editar-confirmar').show();
+        $('.btn-crear-confirmar').hide();
+
+        // Defino las variables con la información que necesito del registro seleccionado.
+        var name = $(this).siblings('strong').find('.span-name').text();
+        var lastname = $(this).siblings('strong').find('.span-lastname').text();
+        var dni = $(this).siblings('strong').find('.span-dni').text();
+
+        // Guardo el id del registro seleccionado para su posterior uso.
+        resourceID = $(this).parent().data('id');
+
+        // Asigno las variables definidas ateriormente a los input del modal.
+        $inputName.val(name);
+        $inputLastname.val(lastname);
+        $inputDNI.val(dni);
+    });
+    
+    $list.on('click', '.btn-eliminar', function(event){
+        // Guardo el id del registro seleccionado para su posterior uso.
+        resourceID = $(this).parent().data('id');
+        deleteResource();
     });
 
+    $('.btn-crear-confirmar').click(function(event){
+        postResource();
+    });
 
-    // LOADING LOGIC
-    var $loading = $('.loadingDiv');
-
-    $(document)
-    .ajaxStart(function () {
-        $loading.show();
-    })
-    .ajaxStop(function () {
-        $loading.hide();
+    $('.btn-editar-confirmar').click(function(event){
+        putResource();
     });
 
 });
